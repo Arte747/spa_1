98e8e42291e5172e094d9131cf7ee60bda1609ea
Arte747 <artemvasil140@gmail.com>
2021-06-09 18:13:07 +0300

    save

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -4,17 +4,32 @@ import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
+	
+	let dialogsData = [
+		{id: 1, name: 'Dimuch'},
+		{id: 2, name: 'Sveta'},
+		{id: 3, name: 'Andrew'},
+		{id: 4, name: 'Stive'},
+	];
+	
+	let messagesData = [
+		{id: 1, message: 'Hello'},
+		{id: 2, message: 'Haw are you?'},
+		{id: 3, message: '=)))'},
+		{id: 4, message: 'Go!!!!!!'},
+	];
+	
+	let dialogsElements = dialogsData.map(d => <Dialog key={d.id} name={d.name} id={d.id} />);
+	
+	let messagesElements = messagesData.map(m => <Message key={m.id} message={m.message}/>);
+	
 	return (
 		<div className={s.dialogs}>
 			<div className={s.dialogsList}>
-				<Dialog name='Dimuch' id='1' />
-				<Dialog name='Sveta' id='2' />
-				<Dialog name='Andrew' id='3' />
+				{dialogsElements}
 			</div>
 			<div className={s.messages}>
-				<Message message='Hello!'/>
-				<Message message='Haw are you?'/>
-				<Message message='=)))'/>
+				{messagesElements}
 			</div>
 		</div>
 	);
--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,16 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+	
+	let postsData = [
+		{id: 1, message: 'Hello world!'},
+		{id: 2, message: 'Haw are you?'},
+		{id: 3, message: 'Yo'},
+		{id: 4, message: '!!!!!!!'},
+	];
+	
+	
+	let postsElements = postsData.map(p => <Post key={p.id} message={p.message} />);
 	return (
 		<div className={s.myPosts}>
 			<div className={s.newPost}>
@@ -12,9 +22,7 @@ const MyPosts = (props) => {
 			</div>
 			
 			<div className={s.postList}>
-				<Post message={'Hello world!'} />
-				<Post message={'Haw are you?'} />
-				<Post message={'Yo'} />
+				{postsElements}
 			</div>
 		</div>
 	);
