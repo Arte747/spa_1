7a84f3840412d432580b219ddddcdb324fda3dc8
Arte747 <artemvasil140@gmail.com>
2021-06-24 15:10:48 +0300

    useEffect

--- my-app/src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,10 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import s from './ProfileStatus.module.css';
 
+// useState, useEffect -> hooks
+
 const ProfileStatusWithHooks = (props) => {
-	console.log(props);
+	
 	let [editMode, setEditMode] = useState(false);
 	let [status, setStatus] = useState(props.status);
+	
+	// выполнится после отрисовки компонента
+	// синхронизируем статус из useState данными из props
+	// второй параметр это зависимости
+	// если propsStatus будет не таким как раньше, запускается effect
+	useEffect(() => {
+		setStatus(props.status);
+	}, [props.status]);
 	
 	const activateEditMode = () => {
 		setEditMode(true);
